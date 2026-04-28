78924d93c04a6f57d212a96b8cddf401d8c31060
laboratorys <359916450@qq.com>
2026-04-28 13:41:29 +0800

    remove log

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,12 +2,5 @@
 import { app } from "../src/app";
 
 export const onRequest = async (context: any) => {
-  // context.request: 请求对象
-  // context.env: 绑定 (如 DB, KV)
-  // context: ExecutionContext (用于 waitUntil 等)
-
-  // 打印一下日志，如果依然报错，我们可以看到 URL 是否异常
-  console.log("Request URL:", context.request.url);
-
   return app.fetch(context.request, context.env, context);
 };
